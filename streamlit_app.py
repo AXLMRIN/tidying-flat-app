f1915e64ad49be55e1856fc80d13129385a2803d
AXLMRIN <axelmorinmail@gmail.com>
2025-08-15 19:47:59 +0200

    remove the dummy text

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,7 +5,4 @@ from pkg import test_function
 st.write("# Gestion des tâches de nettoyage dans la colocation")
 st.write("Liste des tâches à venir :")
 
-cols = st.columns(3)
-cols[0].write("JM")
-cols[1].write("Salon")
-cols[2].button("Done",on_click = test_function())
+st.button("TEST", on_click=test_function)
