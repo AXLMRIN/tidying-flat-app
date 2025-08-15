bd4371b29ce6965979c19cbfc1922773a96421ba
AXLMRIN <axelmorinmail@gmail.com>
2025-08-15 16:06:12 +0200

    deleted the "get_all_tasks" function

--- pkg/general.py
+++ pkg/general.py
@@ -3,19 +3,6 @@ import os
 def test_function():
     print("## TEST ##")
 
-def get_list_of_tasks():
-    """Read the list-of-tasks.txt file and return it's content after cleaning it"""
-    # Read
-    with open("./data/list-of-tasks.txt", "r") as file:
-        all_tasks = file.readlines()
-    # Clean
-    def clean(task : str) -> str:
-        """Remove the dashes and the new lines characters"""
-        return task.replace("-", " ").replace("\n", "")
-    all_tasks = [clean(task) for task in all_tasks]
-    
-    return all_tasks
-
 def name_to_filename(name : str) -> str:
     filename = ''.join(c for c in name if c.isascii())
     return filename + ".txt"
