6c3ff613f1621775a011eb5fc4d8c41147a7964d
AXLMRIN <axelmorinmail@gmail.com>
2025-08-15 21:58:27 +0200

    initial commit

--- pkg/__init__.py
+++ pkg/__init__.py
@@ -3,3 +3,4 @@ from .TASK import *
 from .USER import *
 from .renderer import *
 from .calendar import *
+from .dynamic_functions import *
---
+++ pkg/dynamic_functions.py
@@ -0,0 +1,28 @@
+import pandas as pd
+import streamlit as st
+
+from . import status_index, get_all_users
+
+def dialog_edit_task(id : int, calendar : pd.DataFrame) -> None:
+    st.write(f'ID : {id}')
+    st.write("## Change Status")
+    status = st.radio(
+        label = "Status",
+        options = ["TODO", "DONE", "SKIPPED"],
+        horizontal = True,
+        index = status_index(calendar.loc[calendar["ID"]==id,"Status"].item())
+    )
+    st.write("## Change user")
+    st.write(f"Current user : {calendar.loc[calendar["ID"]==id,"User"].item()}")
+    options = ["/", *[user["name"] for user in get_all_users()]]
+    new_user = st.selectbox(
+        label = "User", 
+        options = options,
+        index = options.index(calendar.loc[calendar["ID"]==id,"User"].item()),
+    )
+    if st.button("Submit"):
+        # UPDATE the table
+        calendar.loc[calendar["ID"]==id,"Status"] = status
+        calendar.loc[calendar["ID"]==id,"User"] = new_user
+        calendar.to_csv("./data/calendar.csv")
+        st.rerun()
