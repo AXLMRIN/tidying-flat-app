4d05ea82bd3dd9a8c3abd1ddde6bd830d3306399
AXLMRIN <axelmorinmail@gmail.com>
2025-08-15 13:38:07 +0200

    read the tasks and print them (test)

--- pkg/general.py
+++ pkg/general.py
@@ -1,6 +1,15 @@
-
 def test_function():
     print("## TEST ##")
+
+def get_list_of_tasks():
+    """Read the list-of-tasks.txt file and return it's content after cleaning it"""
+    # Read
     with open("./data/list-of-tasks.txt", "r") as file:
-        all = file.readlines()
-    print(all)
+        all_tasks = file.readlines()
+    # Clean
+    def clean(task : str) -> str:
+        """Remove the dashes and the new lines characters"""
+        return task.replace("-", " ").replace("\n", "")
+    all_tasks = [clean(task) for task in all_tasks]
+    
+    return all_tasks
--- streamlit_app.py
+++ streamlit_app.py
@@ -1,9 +1,14 @@
 import streamlit as st
 
-from pkg import test_function
+from pkg import test_function, get_list_of_tasks
 
 st.write("# Gestion des tâches de nettoyage dans la colocation")
 st.write("Liste des tâches à venir :")
+
+list_of_tasks = get_list_of_tasks()
+for task in list_of_tasks:
+    st.write(task)
+
 cols = st.columns(3)
 cols[0].write("JM")
 cols[1].write("Salon")
