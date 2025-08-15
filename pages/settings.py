3ba55302fb0cda08a13d5a4c8e8c562af5a5050f
AXLMRIN <axelmorinmail@gmail.com>
2025-08-15 16:59:36 +0200

    change the setting page layout

--- pages/settings.py
+++ pages/settings.py
@@ -1,17 +1,35 @@
 import streamlit as st
 
-from pkg import Task, get_all_tasks, User, get_all_users
+from pkg import Task, get_all_tasks, User, get_all_users, render_user, render_task
 
 st.write("# Settings")
-cols = st.columns(2)
-col_users = cols[0]
-col_tasks = cols[1]
 
+######
+cols_header_users = st.columns([0.8, 0.2])
+cols_header_users[0].write("## Users")
+cols_header_users[1].button(
+    label = "Edit users",
+    key = "users-edit",
+    icon = ":material/edit:",
+    use_container_width = True
+)
 
-col_tasks.write("## Tasks")
-for task in get_all_tasks():
-    col_tasks.write(task.get_data())
+######
+cols_users = st.columns(3)
+for i, user in enumerate(get_all_users()):
+    cols_users[i%3].write(render_user(user), unsafe_allow_html=True)
 
-col_users.write("## Users")
-for user in get_all_users():
-    col_users.write(user.get_data())
+######
+st.write("---")
+cols_header_tasks = st.columns([0.8, 0.2])
+cols_header_tasks[0].write("## Tasks")
+cols_header_tasks[1].button(
+    label = "Edit tasks",
+    key = "tasks-edit",
+    icon = ":material/edit:",
+    use_container_width = True
+)
+
+######
+for i, task in enumerate(get_all_tasks()):
+    st.write(render_task(task), unsafe_allow_html=True)
