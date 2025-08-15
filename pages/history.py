bfb2c64c5ecf0550b68e6dc338c47ff4ee3e549e
AXLMRIN <axelmorinmail@gmail.com>
2025-08-15 21:46:53 +0200

    ...

--- pages/history.py
+++ pages/history.py
@@ -2,8 +2,10 @@ import pandas as pd
 import streamlit as st
 
 from pkg import render_history
-st.write("# History")
 
 calendar = pd.read_csv("./data/calendar.csv").sort_values("Deadline",ascending=False)
+
+st.write("# History")
+
 for iRow in range(len(calendar)):
     st.write(render_history(calendar.iloc[iRow]), unsafe_allow_html=True)
