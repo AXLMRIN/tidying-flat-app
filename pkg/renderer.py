47bb9589653fb1c60c16fc1d1bd27592c34c80e4
AXLMRIN <axelmorinmail@gmail.com>
2025-08-15 16:59:05 +0200

    initial commit

---
+++ pkg/renderer.py
@@ -0,0 +1,61 @@
+from .USER import User
+from .TASK import Task
+
+def render_user(user:User) -> str:
+    data = user.get_data()
+
+    style_div = (
+        "margin: 5px; border-radius: 15px; padding: 3px;"
+        f"background-color: {data["color"]};"
+        "display: flex; flex-direction: column; flex-wrap: nowrap;"
+        "justify-content: center; align-items: center;"
+    )
+    style_name = (
+        "margin: 1px; width: 90%; border-radius: 5px; padding:3px;"
+        "background-color: white; opacity: 0.5;"
+        "color: black; font-size: large; text-align:center;"
+    )
+    style_email = (
+        "margin: 1px; width: 90%; border-radius: 5px; padding:3px;"
+        "background-color: white; opacity: 0.5;"
+        "color: black; text-align:center;"
+    )
+    style_score = (
+        "margin: 1px; width: 90%; border-radius: 5px; padding:3px;"
+        "background-color: white; opacity: 0.5;"
+        "color: black; font-size: large; text-align:center;"
+    )
+    return_str = f'''<div style="{style_div}">
+    <p style="{style_name}">{data["name"]}</p>
+    <p style="{style_email}">{data["email"]}</p>
+    <p style="{style_score}">{data["score"]}</p>
+    </div>'''
+    return return_str
+
+def render_task(task:Task) -> str:
+    data = task.get_data()
+
+    style_div = (
+        "margin: 5px; border-radius: 15px; padding: 3px;"
+        "background-color: grey;"
+        "display: flex; flex-direction: row; flex-wrap: nowrap;"
+        "justify-content: space-between; align-items: center;"
+    )
+    style_name = (
+        "margin: 1px; width: 30%; border-radius: 5px; padding:3px;"
+        "color: black; font-size: large; text-align:center;"
+    )
+    style_description = (
+        "margin: 1px; width: 40%; border-radius: 5px; padding:3px;"
+        "color: black; text-align:center;"
+    )
+    style_frequency = (
+        "margin: 1px; width: 30%; border-radius: 5px; padding:3px;"
+        "color: black; font-size: large; text-align:center;"
+    )
+    return_str = f'''<div style="{style_div}">
+    <p style="{style_name}">{data["name"]}</p>
+    <p style="{style_description}">{data["description"]}</p>
+    <p style="{style_frequency}">every {data["frequency"]} week</p>
+    </div>'''
+    return return_str
