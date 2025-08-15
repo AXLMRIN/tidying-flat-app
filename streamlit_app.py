927326b5bb3752649df1553bf343c5c993aa8754
AXLMRIN <axelmorinmail@gmail.com>
2025-08-15 13:31:20 +0200

    change test function

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,11 +1,10 @@
 import streamlit as st
 
-def test():
-    print("hello world")
+from pkg import test_function
 
 st.write("# Gestion des tâches de nettoyage dans la colocation")
 st.write("Liste des tâches à venir :")
 cols = st.columns(3)
 cols[0].write("JM")
 cols[1].write("Salon")
-cols[2].button("Done",on_click = test())
+cols[2].button("Done",on_click = test_function())
