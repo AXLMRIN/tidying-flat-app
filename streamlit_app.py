470b1c71a2ceac08c49e08ac3a1db13dde753ddb
AXLMRIN <axelmorinmail@gmail.com>
2025-08-15 16:04:51 +0200

    remove lists of tasks

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,13 +1,9 @@
 import streamlit as st
 
-from pkg import test_function, get_list_of_tasks
+from pkg import test_function
 
 st.write("# Gestion des tâches de nettoyage dans la colocation")
 st.write("Liste des tâches à venir :")
-
-list_of_tasks = get_list_of_tasks()
-for task in list_of_tasks:
-    st.write(task)
 
 cols = st.columns(3)
 cols[0].write("JM")
