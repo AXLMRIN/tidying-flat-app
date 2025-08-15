0bf7da16db7658242ff4d6e126063cbda2c18eef
AXLMRIN <axelmorinmail@gmail.com>
2025-08-15 19:47:12 +0200

    define render history

--- pkg/renderer.py
+++ pkg/renderer.py
@@ -1,5 +1,6 @@
 from .USER import User
 from .TASK import Task
+import pandas as pd
 
 def render_user(user:User) -> str:
     data = user.get_data()
@@ -57,5 +58,28 @@ def render_task(task:Task) -> str:
     <p style="{style_name}">{data["name"]}</p>
     <p style="{style_description}">{data["description"]}</p>
     <p style="{style_frequency}">every {data["frequency"]} week</p>
+    </div>'''
+    return return_str
+
+def render_history(entry : pd.Series) -> str:
+    entry : dict = entry.to_dict()
+
+    style_div = (
+        "margin: 5px; border-radius: 15px; padding: 3px;"
+        "background-color: grey;"
+        "display: flex; flex-direction: row; flex-wrap: nowrap;"
+        "justify-content: space-between; align-items: center;"
+    )
+    style_text = (
+        "margin: 1px; width: 20%; border-radius: 5px; padding:3px;"
+        "color: black; font-size: large; text-align:center;"
+    )
+
+    return_str = f'''<div style="{style_div}">
+    <p style="{style_text}">{entry["ID"]}</p>
+    <p style="{style_text}">{entry["Task"]}</p>
+    <p style="{style_text}">{entry["User"]}</p>
+    <p style="{style_text}">{entry["Deadline"]}</p>
+    <p style="{style_text}">{entry["Status"]}</p>
     </div>'''
     return return_str
