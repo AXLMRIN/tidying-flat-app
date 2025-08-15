66ff610b1a4b44275597f42c8608f6291c6a8f58
AXLMRIN <axelmorinmail@gmail.com>
2025-08-15 15:30:45 +0200

    del_user function

--- pkg/general.py
+++ pkg/general.py
@@ -32,3 +32,16 @@ def del_task(filename : str):
 
     else:
         print(f"ERROR: file {filename} does not exist in ./data/tasks")
+
+def del_user(filename : str):
+    list_of_existing_tasks = os.listdir('./data/users')
+    if filename in list_of_existing_tasks:
+        try:
+            os.remove(f"./data/users/{filename}")
+            print(f"File {filename} was successfully deleted")
+        except Exception as e:
+            print((f"ERROR: file {filename} exists in ./data/users but could not "
+                   f"be deleted because:\n{e}"))
+
+    else:
+        print(f"ERROR: file {filename} does not exist in ./data/users")
