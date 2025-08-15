f773661c1e1fc6195f6c48901b88160541e2415b
AXLMRIN <axelmorinmail@gmail.com>
2025-08-15 19:47:02 +0200

    implement method to add events for the coming month

--- pkg/calendar.py
+++ pkg/calendar.py
@@ -14,13 +14,33 @@ def generate_calendar():
     # Sort the values so that the low index (ie 0) is the more recent
     calendar.sort_values("Deadline", ascending = False) 
     for task in get_all_tasks():
-        task_name = task.get_data()["name"]
-        print(task_name)
+        task_name = task["name"]
         sub_calendar = calendar.loc[calendar["Task"] == task_name, :]
         if len(sub_calendar) == 0: 
             # The task does not appear in the history
             last_occurence = today
         else:
             # Fetch the last iteration:
-            last_occurence = sub_calendar.iloc[0]["Deadline"]
-        print(f"{task["name"]} Last occurence : ", last_occurence)
+            last_occurence = pd.Timestamp(sub_calendar.iloc[0]["Deadline"])
+        # Reset the time
+        last_occurence = last_occurence.replace(hour = 0, minute = 0, second = 0, 
+            microsecond = 0)
+        # Add events for the next month
+        next_occurence = last_occurence + pd.Timedelta(weeks = task["frequency"])
+        while next_occurence < today + pd.Timedelta(months = 1):
+            calendar = pd.concat(
+                [
+                    calendar,
+                    pd.DataFrame({
+                        "ID" : len(calendar),
+                        "Task" : task_name,
+                        "Status" : "TODO",
+                        "Deadline" : next_occurence,
+                        "User" : np.nan
+                    }),
+                ], 
+                ignore_index=True
+            )
+            next_occurence = next_occurence + pd.Timedelta(weeks = task["frequency"])
+    calendar.to_csv("./data/calendar.csv", index = False)
+
