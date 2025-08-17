5e137623f5ed4ae77bedb2564645832ee5f1e3d6
AXLMRIN <axelmorinmail@gmail.com>
2025-08-17 17:23:57 +0200

    read data function

--- pkg/gsheets_interactivity.py
+++ pkg/gsheets_interactivity.py
@@ -5,8 +5,7 @@ from . import CONNECTION
 def update_sheet(data : pd.DataFrame, worksheet : str):
     CONNECTION.update(worksheet = worksheet, data = data)
 
-def initialise_calendar():
-    df = pd.read_csv("./data/calendar.csv")
-    df = df.drop(["Unnamed: 0.4","Unnamed: 0.3","Unnamed: 0.2","Unnamed: 0.1","Unnamed: 0"], axis = 1)
-    CONNECTION.create(worksheet = "Calendar", data = df)
-    return "DONE"
+def read_data(worksheet : str, force : bool = False) -> pd.DataFrame:
+    ttl = 1 if force else 3600 
+    calendar = CONNECTION.read(worksheet = worksheet, ttl = ttl)
+    return calendar
