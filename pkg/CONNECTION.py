7465a3846779a8a6a5e0c554cb8304418df6f07d
AXLMRIN <axelmorinmail@gmail.com>
2025-08-21 14:08:12 +0200

    move the filter function

--- pkg/CONNECTION.py
+++ pkg/CONNECTION.py
@@ -1,3 +1,5 @@
+import numpy as np
+import pandas as pd
 import streamlit as st
 from streamlit_gsheets import GSheetsConnection
 
@@ -31,3 +33,45 @@ class Connection:
     def change_worksheet(self, worksheet : str) -> None:
         self.worksheet = worksheet
         self.force_reload()
+
+    def filter_calendar_per_user_and_date(self, user : str = "/") -> pd.DataFrame:
+        calendar = self.data.copy()
+        # Transform the "Deadline" column (str) to Timestamp to be able to use 
+        # the < operator
+        calendar["Deadline"] = self.calendar["Deadline"].apply(pd.Timestamp)
+
+        # Fetch the IDs for the rows corresponding to tasks which due date are
+        # nigh
+        date_max = pd.Timestamp.now() + pd.Timedelta(weeks = 1)
+        date_min = pd.Timestamp.now() - pd.Timedelta(days = 1)
+
+        selected_rows_date = calendar.loc[
+            (calendar["Deadline"] >= date_min) & (calendar["Deadline"] <= date_max),
+            "ID"
+        ]
+
+        # Fetch the IDs for the rows corresponding to tasks associated to a specific
+        # user, if provided
+        if user != "/":
+            selected_rows_user = calendar.loc[
+                calendar["User"] == user,
+                "ID"
+            ]   
+        else:
+            # Take them all because of the way we combine the lists, see below
+            selected_rows_user = calendar["ID"]
+        
+        # Fetch the IDs that have not been completed
+        selected_rows_late = calendar.loc[
+            (calendar["Status"] != "DONE")&(calendar["Status"] != "SKIPPED")&\
+                (calendar["Deadline"] < date_min),
+            "ID"
+        ]
+
+        # Combine IDs from the user and date
+        selected_IDs = [ID for ID in selected_rows_date if ID in selected_rows_user]
+        # Add the IDs for the late tasks, technically, the selected_rows_date and the
+        # selected_rows_late are mutually exclusive
+        selected_IDs += selected_rows_late.to_list()
+
+        return calendar.loc[np.isin(calendar["ID"], selected_IDs), :]
--- pkg/general.py
+++ pkg/general.py
@@ -33,46 +33,6 @@ def del_user(filename : str):
     else:
         print(f"ERROR: file {filename} does not exist in ./data/users")
 
-def filter_calendar(calendar : pd.DataFrame, user : str = "/") -> pd.DataFrame:
-    # Transform the "Deadline" column (str) to Timestamp to be able to use the <
-    # operator
-    calendar["Deadline"] = calendar["Deadline"].apply(pd.Timestamp)
-
-    # Fetch the IDs for the rows corresponding to tasks which due date are nigh
-    date_max = pd.Timestamp.now() + pd.Timedelta(weeks = 1)
-    date_min = pd.Timestamp.now() - pd.Timedelta(days = 1)
-
-    selected_rows_date = calendar.loc[
-        (calendar["Deadline"] >= date_min) & (calendar["Deadline"] <= date_max),
-        "ID"
-    ]
-
-    # Fetch the IDs for the rows corresponding to tasks associated to a specific
-    # user, if provided
-    if user != "/":
-        selected_rows_user = calendar.loc[
-            calendar["User"] == user,
-            "ID"
-        ]   
-    else:
-        # Take them all because of the way we combine the lists, see below
-        selected_rows_user = calendar["ID"]
-    
-    # Fetch the IDs that have not been completed
-    selected_rows_late = calendar.loc[
-        (calendar["Status"] != "DONE")&(calendar["Status"] != "SKIPPED")&\
-            (calendar["Deadline"] < date_min),
-        "ID"
-    ]
-
-    # Combine IDs from the user and date
-    selected_IDs = [ID for ID in selected_rows_date if ID in selected_rows_user]
-    # Add the IDs for the late tasks, technically, the selected_rows_date and the
-    # selected_rows_late are mutually exclusive
-    selected_IDs += selected_rows_late.to_list()
-
-    return calendar.loc[np.isin(calendar["ID"], selected_IDs), :]
-
 def status_index(status) -> int:
     # Make it cleaner
     if status == "TODO": return 0
