ca26b991ea0184f48d6c64ce7d7e6234f2735104
AXLMRIN <axelmorinmail@gmail.com>
2025-08-21 14:04:28 +0200

    give data a type

--- pkg/CONNECTION.py
+++ pkg/CONNECTION.py
@@ -2,12 +2,17 @@ import streamlit as st
 from streamlit_gsheets import GSheetsConnection
 
 class Connection:
-    def __init__(self, worksheet : str):
+    def __init__(self, worksheet : str) -> None:
         # initialise variables
         self.worksheet = worksheet
         self.__connection = st.connection("gsheets", type = GSheetsConnection)
-        # read the data
-        self.data = self.__connection.read(worksheet = worksheet)
+        try:
+            # read the data
+            self.data : pd.DataFrame|None= self.__connection.read(worksheet = worksheet)
+        except:
+            print(("WARNING: force reload - Could not read the worksheet "
+                   f"({self.worksheet})."))
+            self.data : pd.DataFrame|None = None
 
     def force_reload(self):
         self.data = self.__connection.read(worksheet = self.worksheet)
