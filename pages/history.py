d6a85036bb62aa1efb59498fa67a08a6da3c9ddb
AXLMRIN <axelmorinmail@gmail.com>
2025-08-21 15:07:44 +0200

    filter users

--- pages/history.py
+++ pages/history.py
@@ -1,11 +1,14 @@
 import pandas as pd
 import streamlit as st
 
-from pkg import CONNECTION, render_history
+from pkg import CONNECTION, render_history, get_all_users
 st.write("# History")
 
-calendar = CONNECTION.get_all_history()
-
+user = st.selectbox(
+    label = "User", 
+    options= ["/", *[user["name"] for user in get_all_users()]]
+)
+calendar = CONNECTION.get_all_history(user)
 cols = st.columns(4)
 for iRow in range(len(calendar)):
     cols[iRow%4].write(render_history(calendar.iloc[iRow]), unsafe_allow_html=True)
