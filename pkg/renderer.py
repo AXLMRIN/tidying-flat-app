9e96eee30f0cb6166fc19259b2ed00ff5a7a5368
AXLMRIN <axelmorinmail@gmail.com>
2025-08-21 21:24:41 +0200

    customisation

--- pkg/renderer.py
+++ pkg/renderer.py
@@ -75,20 +75,43 @@ def render_history(entry : pd.Series) -> str:
     except:
         status_color = "grey"
 
-    style_div = (
-        "margin: 5px; border-radius: 15px; padding: 3px;"
-       f"background-color: {status_color};"
-        "display: flex; flex-direction: column; flex-wrap: nowrap;"
-        "justify-content: space-between; align-items: center;"
+    if entry["Deadline"] < pd.Timestamp.today():
+        font_color = "#c1121f"
+    else:
+        font_color = "#252630"
+
+    background = (
+        "margin: 2px; border-radius: 15px; padding: 2px;"
+       f"background-color: rgba(255,255,255,0.7); border: 4px solid {status_color};"
+       "display: flex; flex-direction: column; justify-content: space-between;"
+       "align-items: center; flex-wrap: nowrap;"
     )
-    style_text = (
-        "margin: 1px; width: 80%; border-radius: 5px; padding:3px;"
-        "color: black; font-size: large; text-align:center;"
+    title_frame = (
+        "display: flex; flex-direction: row; flex-wrap: nowrap; "
+        "justify-content: space-between;"
+    )
+    task_name = (
+        "font-weight: bold;"
+        "margin: 1px; border-radius: 5px; padding:3px;"
+       f"color: {font_color}; font-size: large; text-align:center;"
+        "flex: 0 1 auto;"
+    )
+    user_name = (
+        "margin: 1px; border-radius: 5px; padding:3px;"
+       f"color: {font_color}; font-size: large; text-align:center;"
+        "flex: 0 1 auto;"
+    )
+    deadline_name = (
+        "margin: 1px; border-radius: 5px; padding:3px;"
+       f"color: {font_color}; font-size: small; text-align:center;"
+        "flex: 0 1 auto;"
     )
 
-    return_str = f'''<div style="{style_div}">
-    <p style="{style_text}">{entry["Task"]}</p>
-    <p style="{style_text}">{entry["User"]}</p>
-    <p style="{style_text}">{pd.Timestamp(entry["Deadline"]).strftime("%Y-%m-%d")}</p>
+    return_str = f'''<div style="{background}">
+        <div style="{title_frame}">
+            <p style="{task_name}">{entry["Task"]}</p>
+            <p style="{user_name}">{entry["User"]}</p>
+        </div>
+        <p style="{deadline_name}">{pd.Timestamp(entry["Deadline"]).strftime("%Y-%m-%d")}</p>
     </div>'''
     return return_str
